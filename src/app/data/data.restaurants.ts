be986ac156dcbd70971b0e88740b6856d73bc67f
jesusmen <jesusmengibardelom96@gmail.com>
2020-01-28 13:22:41 +0100

    Initialiting firestore functionality

---
+++ src/app/data/data.restaurants.ts
@@ -0,0 +1,26 @@
+export const RESTAURANTS = [
+    {
+      mail: "prueba@prueba.com",
+      name: "Burger king",
+      type: "Burgers",
+      visited: true,
+      photo: "",
+      logo: "",
+      comment: "",
+      opinion: "",
+      priceRange: "15-30",
+      district: "Xirivella"
+    },
+    {
+      mail: "jesusmengibardelom96@gmail.com",
+      name: "Domino's pizza",
+      type: "Pizza",
+      visited: false,
+      photo: "",
+      logo: "",
+      comment: "",
+      opinion: "",
+      priceRange: "30-45",
+      district: "Aldaia"
+    }
+  ];
